d0ff68399e60b972a4c561e8173e29f90c4bf260
Jason Leach <jlspursfan@gmail.com>
2019-06-24 16:21:02 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Jason"
+  end
+
+  get '/hometown' do
+    "My hometown is Cleburne"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is The Thong Song"
+  end
+
 end
