9a527fafa1371607a927996fdd4490074d77d64c
Michael J Maggs <michaelmaggs@gmail.com>
2019-06-24 13:20:16 -0700

    completed sinatra route lab

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/something' do
+        "Hello World new"
+      end
+
+    get '/name' do 
+        "My name is Michael"
+    end
+
+    get '/hometown' do 
+        "My hometown is Seattle"
+    end
+
+    get '/favorite-song' do 
+        "My favorite song is \'Anuerysm\'"
+    end
+
 end
